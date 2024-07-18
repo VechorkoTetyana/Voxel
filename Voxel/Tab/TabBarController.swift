ec922446372c8316d99c7be3fcf4f34f84c19904
VechorkoTetyana <44574436+VechorkoTetyana@users.noreply.github.com>
2024-07-19 01:36:11 +0200

    Branch-13 TabBar Settings large Lbl

---
+++ Voxel/Tab/TabBarController.swift
@@ -0,0 +1,43 @@
+import UIKit
+import DesignSystem
+import VoxelSettings
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupViewControllers()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .background
+        tabBar.barTintColor = .background
+        tabBar.tintColor = .accent
+    }
+    
+    private func setupViewControllers() {
+        let contacts = UIViewController()
+        contacts.tabBarItem = Tab.contacts.tabBarItem
+        
+        let calls = UIViewController()
+        calls.tabBarItem = Tab.calls.tabBarItem
+        
+        let chats = UIViewController()
+        chats.tabBarItem = Tab.chats.tabBarItem
+
+        let settings = SettingsViewController()
+        let settingsNav = UINavigationController(rootViewController: settings)
+        settings.tabBarItem = Tab.settings.tabBarItem
+        settings.title = Tab.settings.tabBarItem.title
+        
+        viewControllers = [
+            contacts,
+            calls,
+            chats,
+            settingsNav
+        ]
+        
+        selectedViewController = settingsNav
+    }
+}
